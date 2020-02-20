4a06f940ddb7e18f6b16cb5f448a2ba6d3f545bd
M.O Akindayo <matthewakinse@gmail.com>
2020-02-20 11:24:34 +0200

    Reusable - looping the spans

--- js/script.js
+++ js/script.js
@@ -1,6 +1,5 @@
 // M.O Akins: Best wishes
 
-
 // The period calculations
 "use strict"
 
@@ -17,27 +16,30 @@ function Counter(newyear) {
 
 // Animating the counter
 function startCounter(id, newyear) {
-  let timerInterval = setInterval(function () {
-    let count = document.getElementById(id);
-    let rounds = Counter(newyear);
-
-    count.innerHTML = '<span>' + rounds.days + '</span>' + '<div>' + ':' + '</div>'
-      + '<span>' + rounds.hours + '</span>' + '<div>' + ':' + '</div>'
-      + '<span>' + rounds.minutes + '</span>' + '<div>' + ':' + '</div>'
-      + '<span>' + rounds.seconds + '</span>';
-
-
-    //check for end of the counter
-    if (rounds.total < 1) {
-      clearInterval(timerInterval);
-      count.innerHTML = '<span>0</span><div>:</div><span>0</span><div>:</div><span>0</span><div>:</div><span>0</span>';
+  let roundInterval = setInterval(function () {
+    let count = document.getElementById(id), rounds = Counter(newyear);
+    let spans = 4, colons = 2;
+    if (rounds.total > 1) {
+      count.innerHTML = '<span>' + rounds.days + '</span>' + '<div>' + ':' + '</div>'
+        + '<span>' + rounds.hours + '</span>' + '<div>' + ':' + '</div>'
+        + '<span>' + rounds.minutes + '</span>' + '<div>' + ':' + '</div>'
+        + '<span>' + rounds.seconds + '</span>'
     }
-
-  }, 1000);
+    else {
+      //check for end of the counter
+      for (let i = 0; i < spans; i++) {
+        count.innerHTML += '<span>0</span>';
+        if (i > colons) { break; }
+        count.innerHTML += '<div>:</div>';
+        clearInterval(roundInterval);
+      }
+      //count.innerHTML = '<span>0</span><div>:</div><span>0</span><div>:</div><span>0</span><div>:</div><span>0</span>';
+    }
+  }, 1000)
 }
 
 window.addEventListener('load', function () {
-  var newyear = new Date("April 12, 2020 00:00:00");
+  var newyear = new Date("January 1, 2020 00:00:00");
   startCounter("count", newyear);
 })
 
