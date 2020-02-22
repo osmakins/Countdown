98a01546bacf628c5eda4b06c908135a46fdb595
M.O Akindayo <matthewakinse@gmail.com>
2020-02-22 12:24:15 +0200

    Add snow canvas - animate snowflakes

--- js/script.js
+++ js/script.js
@@ -57,4 +57,64 @@ window.addEventListener('load', function () {
   startCounter("count", newyear);
 })
 
+// Adding the snow effect
+window.addEventListener('load', function () {
+  // get canvas context
+  var canvas = document.getElementById("snow");
+  var context = canvas.getContext('2d');
 
+  // set the canvas dimesions
+  var W = window.innerWidth;
+  var H = window.innerHeight;
+  canvas.width = W;
+  canvas.height = H;
+
+  // Generate the snowflakes and apply attributes
+  var maxf = 100; // maximum flakes
+  var flakes = [];
+
+  //loop through to populate flakes array
+  for (var i = 0; i < maxf; i++) {
+    flakes.push({
+      x: Math.random() * W, // horizontal random position
+      y: Math.random() * H, // vertical random position
+      r: Math.random() * 5 + 2, //minimum size of 2px and maximum of 7px
+      d: Math.random() + 1 //density of the flakes
+    })
+  }
+
+  // draw flakes onto canvas
+  function drawFlakes() {
+    context.clearRect(0, 0, W, H);
+    context.fillStyle = "white";
+    context.beginPath();
+    for (let i = 0; i < maxf; i++) {
+      var f = flakes[i];
+      context.moveTo(f.x, f.y);
+      context.arc(f.x, f.y, f.r, 0, Math.PI * 2, true);
+    }
+    context.fill();
+    moveFlakes();
+  }
+
+  var angle = 0;
+  // animate the flakes
+  function moveFlakes() {
+    angle += 0.01;
+    for (let i = 0; i < maxf; i++) {
+      // store current flake
+      var f = flakes[i];
+
+      // update X and Y coordinates of each snowflake - cos x graph path
+      f.y += Math.pow(f.d, 2) + 1;
+      f.x += Math.cos(angle) * 2;
+
+      // if the snowflake reaches the bottom, send a new one to the top
+      if (f.y > H) {
+        flakes[i] = { x: Math.random() * W, y: 0, r: f.r, d: f.d };
+      }
+    }
+  }
+
+  setInterval(drawFlakes, 25);
+})
